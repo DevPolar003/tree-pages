4c331034f00a29535e4d78b694cc9c4f8c211c98
Devpolar003 <polardeveloper11@proton.me>
2026-06-08 11:46:12 -0300

    Creating new classes and relations

--- src/main/java/dev/morgan/treepages/models/Tree.java
+++ src/main/java/dev/morgan/treepages/models/Tree.java
@@ -1,4 +1,13 @@
 package dev.morgan.treepages.models;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table (name = "Tree")
 public class Tree {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
 }
